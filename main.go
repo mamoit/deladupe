d1f90d5cd2b09144defda294aa045ee89d9f60d2
Miguel Almeida <migaxmoitax@gmail.com>
2020-06-07 15:49:49 +0100

    Code review from a friend :)

--- main.go
+++ main.go
@@ -11,128 +11,155 @@ import (
 	"strconv"
 )
 
-type DedupDir struct {
-	parent   *DedupDir
-	children []*DedupDir
-	files    []*DedupFile
-}
-
-type DedupFile struct {
-	hash   string
-	path   string
-	parent *DedupDir
-}
-
-var hmap map[int64]map[string][]DedupFile
-var min_size uint64
-
-func visit_target(path string, info os.FileInfo, err error) error {
-	if err != nil {
-		log.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
-		return err
-	}
-
-	// Do not look into symlinks
-	// Do not look into directories
-	if info.Mode()&(os.ModeSymlink|os.ModeDir) != 0 {
-		return nil
-	}
-
-	// check if file is too small
-	if uint64(info.Size()) < min_size {
-		return nil
-	}
-
+// Helper functions
+func computeHash(path string) (string, error) {
 	// open file
 	f, err := os.Open(path)
 	if err != nil {
-		log.Print(err)
-		return nil
+		log.Println(err)
+		return "", err
 	}
 	defer f.Close()
 
 	// calculate sha256
 	h := sha256.New()
 	if _, err := io.Copy(h, f); err != nil {
-		log.Print(err)
+		return "", err
 	}
-	hash := hex.EncodeToString(h.Sum(nil))
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
 
-	_, ok := hmap[info.Size()]
-	if !ok {
-		hmap[info.Size()] = make(map[string][]DedupFile)
+type DedupFile struct {
+	hash string
+	path string
+}
+
+type dedupContentTrack struct {
+	filesBySize map[int64]*hashes
+}
+type hashes struct {
+	filesByHash map[string][]DedupFile
+}
+
+// Deduper methods
+type Deduper struct {
+	hmap    dedupContentTrack
+	minSize uint64
+}
+
+func NewDeduper(minSize uint64) *Deduper {
+	hmap := make(map[int64]*hashes)
+	return &Deduper{
+		hmap:    dedupContentTrack{hmap},
+		minSize: minSize,
 	}
-	hmap[info.Size()][hash] = append(hmap[info.Size()][hash], DedupFile{hash, path, nil})
+}
+
+func (d *Deduper) trackDedupFile(size int64, hash string, file DedupFile) {
+	_, ok := d.hmap.filesBySize[size]
+	if !ok {
+		h := &hashes{filesByHash:make(map[string][]DedupFile)}
+		d.hmap.filesBySize[size] = h
+	}
+	defupFileList := d.hmap.filesBySize[size].filesByHash[hash]
+	d.hmap.filesBySize[size].filesByHash[hash] = append(defupFileList, file)
+}
+
+func (d *Deduper) getDedupFiles(size int64, hash string) []DedupFile {
+	files , ok := d.hmap.filesBySize[size].filesByHash[hash]
+	if !ok {
+		return nil
+	}
+	return files
+}
+
+func (d *Deduper) shouldVisit(info os.FileInfo) bool {
+	// Do not look into symlinks
+	// Do not look into directories
+	if info.Mode()&(os.ModeSymlink|os.ModeDir) != 0 {
+		return false
+	}
+
+	// check if file is too small
+	if uint64(info.Size()) < d.minSize {
+		return false
+	}
+	return true
+}
+
+func (d *Deduper) visitTarget(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		log.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+		return err
+	}
+	if !d.shouldVisit(info) {
+		return nil
+	}
+
+	// calculate sha256
+	hash, err := computeHash(path)
+	if err != nil {
+		return err
+	}
+
+	dedup := DedupFile{
+		hash:   hash,
+		path:   path,
+	}
+	d.trackDedupFile(info.Size(), hash, dedup)
 
 	return nil
 }
 
-func visit_source(path string, info os.FileInfo, err error) error {
+func (d *Deduper) visitSource(path string, info os.FileInfo, err error) error {
 	if err != nil {
 		log.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 		return err
 	}
 
-	// Do not look into symlinks
-	// do not look into directories
-	if info.Mode()&(os.ModeSymlink|os.ModeDir) != 0 {
+	if !d.shouldVisit(info) {
 		return nil
 	}
 
-	// check if file is too small
-	if uint64(info.Size()) < min_size {
-		return nil
-	}
-
-	// stop if there is no file with the same size
+	// stop if there is no file in target with the same size
 	size := info.Size()
-	_, ok := hmap[size]
+	_, ok := d.hmap.filesBySize[size]
 	if !ok {
 		return nil
 	}
 
-	// open file
-	f, err := os.Open(path)
-	if err != nil {
-		log.Print(err)
-		return nil
-	}
-	defer f.Close()
-
 	// calculate sha256
-	h := sha256.New()
-	if _, err := io.Copy(h, f); err != nil {
-		log.Print(err)
+	hash, err := computeHash(path)
+	if err != nil {
+		return err
 	}
-	hash := hex.EncodeToString(h.Sum(nil))
 
-	_, ok = hmap[size][hash]
-	if ok {
+	files := d.getDedupFiles(size, hash)
+	if files != nil {
 		fmt.Println(size, path)
-		for file := range hmap[size][hash] {
-			fmt.Println("- ", hmap[size][hash][file].path)
+		for file := range files {
+			fmt.Println("- ", files[file].path)
 		}
 	}
 	return nil
 }
 
 func main() {
-	hmap = make(map[int64]map[string][]DedupFile)
-
-	min_size = 0
+	var minSize uint64
 	if len(os.Args) >= 4 {
-		min_size, _ = strconv.ParseUint(os.Args[3], 10, 64)
+		minSize, _ = strconv.ParseUint(os.Args[3], 10, 64)
 	}
+	deduper := NewDeduper(minSize)
 
 	path := os.Args[2]
-	err := filepath.Walk(path, visit_target)
+	err := filepath.Walk(path, deduper.visitTarget)
 	if err != nil {
 		fmt.Printf("error walking the path %q: %v\n", path, err)
 		return
 	}
 
 	path = os.Args[1]
-	err = filepath.Walk(path, visit_source)
+	err = filepath.Walk(path, deduper.visitSource)
 	if err != nil {
 		fmt.Printf("error walking the path %q: %v\n", path, err)
 		return
